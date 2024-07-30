9ad3de42dadcf7e67bb472052eafd9f9df357645
renanwurster <totozerameister@gmail.com>
2024-07-30 15:58:11 +0200

    Initial Commit

---
+++ MeineErsteApp/MeineErsteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeineErsteAppApp.swift
+//  MeineErsteApp
+//
+//  Created by Renan Wurster on 30.07.24.
+//
+
+import SwiftUI
+
+@main
+struct MeineErsteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
