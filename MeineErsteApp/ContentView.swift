50b827e4649148dfe4ee985f15961473afabfdc1
renanwurster <totozerameister@gmail.com>
2024-07-31 10:51:03 +0200

    add image, scrollview

--- MeineErsteApp/ContentView.swift
+++ MeineErsteApp/ContentView.swift
@@ -29,13 +29,44 @@ struct PersonView: View {
 
 struct ContentView: View {
     
-    var persons = [Person(name: "Renan", age: 34, height: 1.79), Person(name: "Amanda", age: 34, height: 1.68)]
+    var persons = [Person(name: "Renan", age: 34, height: 1.79), Person(name: "Amanda", age: 34, height: 1.68), Person(name: "Alice", age: 2, height: 0.92), Person(name: "Vo Marli", age: 60, height: 1.76), Person(name: "Vo Jordan", age: 61, height: 1.77)]
         
     var body: some View{
+        Button{
+            print("Button wurde geklickt")
+        } label: {
+            Text("Click mich!")
+        }
         VStack{
+            ScrollView{
             PersonView(person: persons[0])
             Divider()
             PersonView(person: persons[1])
+            Divider()
+            PersonView(person: persons[2])
+            Divider()
+            PersonView(person: persons[3])
+            Divider()
+            PersonView(person: persons[4])
+            Divider()
+            PersonView(person: persons[3])
+            Divider()
+            PersonView(person: persons[4])
+                Divider()
+                PersonView(person: persons[3])
+                Divider()
+                PersonView(person: persons[4])
+                Divider()
+                PersonView(person: persons[3])
+                Divider()
+                PersonView(person: persons[4])
+            }
+        }
+        ZStack{
+            Image("beach")
+                .resizable()
+                .frame(width: 300, height: 300)
+            Text("Hallo")
         }
         
     }
