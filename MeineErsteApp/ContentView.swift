a2b2565174a6aba6598d09af13872445481bb69b
renanwurster <totozerameister@gmail.com>
2024-07-31 10:18:19 +0200

    add ui test

--- MeineErsteApp/ContentView.swift
+++ MeineErsteApp/ContentView.swift
@@ -7,15 +7,37 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+struct Person{
+    var name: String
+    var age: Int
+    var height: Double
+}
+
+struct PersonView: View {
+    var person: Person
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("Name: \(person.name)")
+            Text("Age: \(person.age)")
+            Text("Height: \(person.height)")
         }
-        .padding()
+    }
+}
+
+
+
+struct ContentView: View {
+    
+    var persons = [Person(name: "Renan", age: 34, height: 1.79), Person(name: "Amanda", age: 34, height: 1.68)]
+        
+    var body: some View{
+        VStack{
+            PersonView(person: persons[0])
+            Divider()
+            PersonView(person: persons[1])
+        }
+        
     }
 }
 
